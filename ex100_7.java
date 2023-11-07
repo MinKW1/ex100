583594c7305c2d8382f61a034859126d6091c86b
MinKW1 <74577665+MinKW1@users.noreply.github.com>
2023-11-07 21:00:54 +0900

    Add files via upload

--- ex100_7.java
+++ ex100_7.java
@@ -1,9 +1,19 @@
 package test1;
 
+import java.util.Scanner;
+
 public class ex100_7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		
+		System.out.print(a);
+		
+		scanner.close();
+		
 
 	}
 
